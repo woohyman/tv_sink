d74e7e65b5123ac9c0b2bef5b8968bb3324c13d1
wjw <824838259@qq.com>
2022-03-29 13:13:22 +0800

    【add】1.根据播放状态回调来更新界面

--- lib/business/PlayControlManager.dart
+++ lib/business/PlayControlManager.dart
@@ -3,33 +3,31 @@ import 'package:tvSink/util/log.dart';
 
 class PlayControlManager {
   PlayControlManager._();
+
   //第一种方式调用
   factory PlayControlManager() {
     return instance;
   }
+
   //第二种方式调用
   static PlayControlManager instance = PlayControlManager._();
 
-  final _controls = <String,VlcPlayerController>{};
+  final _controls = <String, VlcPlayerController>{};
 
-  void setCurTvChannel(String dataSource,VlcPlayerController vlcPlayerController){
+  void setCurTvChannel(
+      String dataSource, VlcPlayerController vlcPlayerController) {
     _controls[dataSource] = vlcPlayerController;
   }
 
-  void playCurTv(String dataSource){
-    _controls.forEach((key, controller) {
-      // try{
-      //   if(dataSource != key){
-      //     controller.dispose();
-      //   }
-      // }catch(err){
-      //   logger.e("err = $err");
-      // }
-      _controls[dataSource]?.play();
-    });
+  void removeChannel(String dataSource) {
+    _controls.remove(dataSource);
   }
 
-  VlcPlayerController? getCurController(String dataSource){
+  void playCurTv(String dataSource) {
+    _controls[dataSource]?.play();
+  }
+
+  VlcPlayerController? getCurController(String dataSource) {
     return _controls[dataSource];
   }
 }
--- lib/widgets/PlayerWrapper.dart
+++ lib/widgets/PlayerWrapper.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'package:provider/provider.dart';
 import 'package:tvSink/model/bean/TvResource.dart';
+import 'package:tvSink/util/log.dart';
 
 import '../business/PlayControlManager.dart';
-import '../util/log.dart';
 
 class PlayerWrapper extends StatefulWidget {
   late String _dataSource;
@@ -19,76 +19,102 @@ class PlayerWrapper extends StatefulWidget {
 }
 
 class _PlayerWrapperState extends State<PlayerWrapper> {
-  int _active = 0;
   late VlcPlayerController _videoPlayerController;
+  late List array;
+  PlayingState _playingState = PlayingState.initializing;
 
   void _handleTap(CommonData commonData) {
-    logger.d("_handleTap => ???????????????//  $_active");
-    if(_active == 0) _active = 1;
-    _videoPlayerController.isPlaying().then((value) => {
-    if (value == false) {
-      commonData.setTvChannel(_videoPlayerController.dataSource),
-      logger.d("===> kaishibofangship"),
-      PlayControlManager.instance.playCurTv(_videoPlayerController.dataSource),
-    } else{
-      _videoPlayerController.stop(),
-      logger.d("zantingbofangshiping"),
-    }
-        }
-    );
-
+    if(_playingState == PlayingState.stopped){
+      PlayControlManager.instance
+          .playCurTv(_videoPlayerController.dataSource);
+    }else if(_playingState == PlayingState.playing){
+      _videoPlayerController.stop();
+    };
   }
-  late List array;
+
   @override
   void initState() {
     array = widget._dataSource.split(",");
-    logger.i("initState|title = ${array[0]}");
     String dataSource1 = widget._dataSource;
-    if(array.length >1) dataSource1 = dataSource1.split(",")[1];
-    _videoPlayerController = PlayControlManager.instance.getCurController(dataSource1)
-        ??VlcPlayerController.network(
-          dataSource1,
-          hwAcc: HwAcc.full,
-          autoPlay: false,
-          options: VlcPlayerOptions(),
-        );
-    PlayControlManager.instance.setCurTvChannel(dataSource1, _videoPlayerController);
+    if (array.length > 1) dataSource1 = dataSource1.split(",")[1];
+    _videoPlayerController =
+        PlayControlManager.instance.getCurController(dataSource1) ??
+            VlcPlayerController.network(
+              dataSource1,
+              hwAcc: HwAcc.full,
+              autoPlay: false,
+              options: VlcPlayerOptions(),
+            );
+    PlayControlManager.instance
+        .setCurTvChannel(dataSource1, _videoPlayerController);
+
+    _videoPlayerController.addListener(() {
+      if(_videoPlayerController.value.playingState == PlayingState.playing && _videoPlayerController.value.position.inMilliseconds <= 0){
+        logger.e("inMilliseconds => ${_videoPlayerController.value.position.inMilliseconds}");
+        return;
+      }
+      if (_playingState != _videoPlayerController.value.playingState) {
+        setState(
+            () => _playingState = _videoPlayerController.value.playingState);
+      }
+    });
     super.initState();
   }
 
-  Widget getStateView(){
-    if(_active == 0){
-      return Icon(Icons.play_circle_filled);
-    }else{
-      return CircularProgressIndicator(
-        backgroundColor: Colors.grey[200],
-        valueColor: AlwaysStoppedAnimation(Colors.blue),
-      );
+  Widget getStateView() {
+    logger.e("getStateView ===> $_playingState");
+    switch (_playingState) {
+      case PlayingState.initializing:
+        return Text("");
+      case PlayingState.initialized:
+        return Icon(Icons.play_circle_filled);
+      case PlayingState.stopped:
+        return Icon(Icons.play_circle_filled);
+      case PlayingState.paused:
+        return Icon(Icons.play_circle_filled);
+      case PlayingState.ended:
+        return Icon(Icons.play_circle_filled);
+      case PlayingState.buffering:
+        return CircularProgressIndicator();
+      case PlayingState.playing:
+        return Text("");
+      case PlayingState.recording:
+        return Text("");
+      case PlayingState.error:
+        return Icon(Icons.play_circle_filled);
     }
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _videoPlayerController.startRendererScanning();
+    await _videoPlayerController.dispose();
+    PlayControlManager.instance.removeChannel(widget._dataSource);
   }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<CommonData>(builder: (ctx, commonData, child) {
       return Stack(
-        alignment: Alignment.center ,
+        alignment: Alignment.center,
         children: <Widget>[
-          getStateView(),
           InkWell(
-            onTap: () => {_handleTap(commonData)},
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(array[0]??""),
-              VlcPlayer(
-                controller: _videoPlayerController,
-                aspectRatio: 16 / 9,
-                placeholder: const Center(child: CircularProgressIndicator()),
-              ),
-      ],
-      )
-          ),
-      ],
+              onTap: () => {_handleTap(commonData)},
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(array[0] ?? ""),
+                  VlcPlayer(
+                    controller: _videoPlayerController,
+                    aspectRatio: 16 / 9,
+                    placeholder:
+                        const Center(child: CircularProgressIndicator()),
+                  ),
+                ],
+              )),
+          getStateView(),
+        ],
       );
     });
   }
