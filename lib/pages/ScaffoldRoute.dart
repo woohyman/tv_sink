06bae1149e247d4a10553705896de439c758b8b9
wjw <824838259@qq.com>
2022-04-20 16:00:38 +0800

    【upd】1.升级功能封装起来

--- lib/pages/ScaffoldRoute.dart
+++ lib/pages/ScaffoldRoute.dart
@@ -40,6 +40,16 @@ ItemScrollController? getScrollController(int _index) {
   }
 }
 
+ItemScrollController? getScrollController2(int _index) {
+  if (_index == 0) {
+    return _chineseController.isAttached ? _chineseController : null;
+  } else if (_index == 1) {
+    return _foreignController.isAttached ? _foreignController : null;
+  } else {
+    return null;
+  }
+}
+
 class _ScaffoldRouteState extends State<ScaffoldRoute> {
   final ValueNotifier<int> _selectedIndex = ValueNotifier<int>(0);
   PageController? _pageController;
@@ -112,147 +122,13 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> {
 
     List<String> _list = _preferences?.getStringList("xx") ?? [];
     List<String> _iconlist = _preferences?.getStringList("icon") ?? [];
-    if (getScrollController(index) == null) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-              child: ScrollablePositionedList.builder(
-            addAutomaticKeepAlives: true,
-            // padding: EdgeInsets.only(left: 0, right: 0, top: 50, bottom: 50),
-            initialScrollIndex: _initTabIndex,
-            itemCount: _tvList.length,
-            itemBuilder: (BuildContext context, int innerIndex) {
-              List<DropdownMenuItem<String>>? myItems = [];
-              Set sets = commonData.getSourceSet(index, innerIndex);
-              sets.toList().asMap().forEach((key, value) {
-                myItems.add(DropdownMenuItem<String>(
-                  value: value,
-                  child: Text("直播源${key + 1}"),
-                ));
-              });
 
-              return Card(
-                color: commonData.getTvName() ==
-                        commonData.getBeanByIndex(index, innerIndex)
-                    ? Colors.lightBlue.shade200
-                    : Colors.lightBlue.shade100,
-                //z轴的高度，设置card的阴影
-                elevation: commonData.getTvName() ==
-                        commonData.getBeanByIndex(index, innerIndex)
-                    ? 20.0
-                    : 0.0,
-                //设置shape，这里设置成了R角
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
-                //对Widget截取的行为，比如这里 Clip.antiAlias 指抗锯齿
-                clipBehavior: Clip.antiAlias,
-                semanticContainer: false,
-                child: InkWell(
-                    onTap: () => {
-                          if (_list.last !=
-                              commonData.getBeanByIndex(index, innerIndex))
-                            {
-                              _list.add(
-                                  commonData.getBeanByIndex(index, innerIndex)),
-                              _iconlist.add(
-                                  commonData.getIconUrl(index, innerIndex)),
-                              _preferences?.setStringList("xx", _list),
-                              _preferences?.setStringList("tvLogo", _list),
-                            },
-                          commonData.setTvChannel(
-                              commonData.getBeanByIndex(index, innerIndex),
-                              index),
-                        },
-                    child: Padding(
-                      child: Flex(
-                        direction: Axis.horizontal,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 5),
-                            child: Expanded(
-                              flex: 1,
-                              child: getImageProviderByUrl(index, innerIndex),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 5,
-                            child: Text(
-                              commonData.getBeanByIndex(index, innerIndex),
-                              style: commonData.getTvName() ==
-                                      commonData.getBeanByIndex(
-                                          index, innerIndex)
-                                  ? TextStyle(color: Colors.red, fontSize: 18)
-                                  : TextStyle(
-                                      color: Colors.black, fontSize: 14),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 3,
-                            child: Visibility(
-                              visible: commonData
-                                      .getSourceSet(index, innerIndex)
-                                      .length >
-                                  1,
-                              child: DropdownButton<String>(
-                                value: commonData.getLiveSource(commonData
-                                    .getBeanByIndex(index, innerIndex)),
-                                onChanged: (value) {
-                                  setState(() {
-                                    commonData.setLiveSource(
-                                        commonData.getBeanByIndex(
-                                            index, innerIndex),
-                                        value);
-                                  });
-                                },
-                                items: myItems,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: InkWell(
-                                onTap: () => {
-                                  setState(() {
-                                    if (commonData.iscotain(commonData
-                                        .getBeanByIndex(index, innerIndex))) {
-                                      commonData.removeurl(commonData
-                                          .getBeanByIndex(index, innerIndex));
-                                    } else {
-                                      commonData.addcollect(commonData
-                                          .getBeanByIndex(index, innerIndex));
-                                    }
-                                  })
-                                },
-                                child: commonData.iscotain(commonData
-                                        .getBeanByIndex(index, innerIndex))
-                                    ? Icon(Icons.favorite)
-                                    : Icon(Icons.favorite_border),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      padding: EdgeInsets.only(
-                          left: 0, right: 0, top: 12, bottom: 12),
-                    )),
-              );
-            },
-          ))
-        ],
-      );
-    }
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Expanded(
             child: ScrollablePositionedList.builder(
-          addAutomaticKeepAlives: true,
           itemScrollController: getScrollController(index),
-          // padding: EdgeInsets.only(left: 0, right: 0, top: 50, bottom: 50),
           initialScrollIndex: _initTabIndex,
           itemCount: _tvList.length,
           itemBuilder: (BuildContext context, int innerIndex) {
@@ -382,18 +258,13 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> {
     CommonData commonData = Provider.of<CommonData>(context, listen: true);
     Position _position = commonData.getPositionByName();
     try {
-      logger.e(
-          "_position ==>${commonData.index} +> ${_position.tabIndex} : ${_position.listIndex}");
       if (commonData.index < 0) {
         _selectedIndex.value = _position.tabIndex;
         _pageController?.jumpToPage(_position.tabIndex);
-        if (_pageController?.position == 0) {
-          if (_chineseController.isAttached)
-            _chineseController.jumpTo(index: _position.listIndex);
-        } else {
-          if (_foreignController.isAttached)
-            _foreignController.jumpTo(index: _position.listIndex);
-        }
+        getScrollController2(_position.tabIndex)
+            ?.jumpTo(index: _position.listIndex);
+        logger.e(
+            "_position ==>${commonData.index} +> ${_position.tabIndex} : ${_position.listIndex} | ${getScrollController(_position.tabIndex)}");
       }
     } catch (err, stack) {
       logger.e("_position ==>$err : $stack");
