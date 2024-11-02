3a9ec871b260c4ecb330ff3cfd45abc1b698fdbf
吴剑舞 <824838259@qq.com>
2024-11-02 12:34:40 +0800

    [修改版本]0.0.33

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"af0f0d559c8a87d912a20971bbd84afc80a54b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2169554123"
+    serviceWorkerVersion: "594543352"
   }
 });
